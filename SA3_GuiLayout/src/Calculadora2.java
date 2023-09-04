6a1af110f385d6431f8ba778730d45187b6deaba
epicestudar <viniciusftza@gmail.com>
2023-09-04 16:41:24 -0300

    commit

---
+++ SA3_GuiLayout/src/Calculadora2.java
@@ -0,0 +1,10 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Calculadora2 extends JPanel{
+    public Calculadora2() {
+        super();
+         this.add(new JLabel("Calculadora 2"));
+        
+    }
+}
