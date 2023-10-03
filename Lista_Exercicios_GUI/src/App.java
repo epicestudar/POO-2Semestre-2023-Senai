48b911687cdaaab30c1b093db0feacafa74a069f
epicestudar <viniciusftza@gmail.com>
2023-10-03 16:07:08 -0300

    codando

--- Lista_Exercicios_GUI/src/App.java
+++ Lista_Exercicios_GUI/src/App.java
@@ -1,8 +1,9 @@
 import Exercicios_Principais.Exercicio1;
 import Exercicios_Principais.Exercicio3;
+import Exercicios_Principais.Exercicio5;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       new Exercicio3();
+       new Exercicio5();
     }
 }
--- Lista_Exercicios_GUI/src/Exercicios_Principais/Exercicio5.java
+++ Lista_Exercicios_GUI/src/Exercicios_Principais/Exercicio5.java
@@ -14,14 +14,36 @@ public class Exercicio5 extends JFrame{
         JPanel card1 = new JPanel();
         card1.add(new JLabel("Jogo Quiz - Seja bem Vindo"));
         JButton but1 = new JButton("Start"); // botão card 1
+        card1.add(but1);
         mainCard.add(card1, "Start");
 
         // card2 - minha primeira pergunta
-        JPanel card2 = new JPanel();
-        card2.add(new JLabel("Quem foi campeão da copa do brasil?"));
+        JPanel card2 = new JPanel(new BorderLayout());
+        JPanel painelVazio = new JPanel();
+        JPanel painelVazio2 = new JPanel();
+        JPanel painelPergunta = new JPanel();
+        JPanel painelBotao = new JPanel();
+        JLabel pergunta1 = new JLabel("Quem foi o campeão da copa do brasil?");
         JTextField resposta1 = new JTextField(10);
-        card2.add(resposta1);
+        painelPergunta.add(pergunta1);
+        painelPergunta.add(resposta1);
         JButton but2 = new JButton("Avançar");
-        card2.add(but2);
+        painelBotao.add(but2);
+        card2.add(painelVazio, BorderLayout.NORTH);
+        card2.add(painelVazio2, BorderLayout.NORTH);
+        card2.add(painelPergunta, BorderLayout.CENTER);
+        card2.add(painelBotao, BorderLayout.SOUTH);
+        mainCard.add(card2, "Start");
+
+        
+
+        // set do frame
+        this.setDefaultCloseOperation(2);
+         this.setBounds(100, 100, 300, 300);
+        this.setVisible(true);
+
+         but1.addActionListener(e-> {
+            cl.show(mainCard, but1.getText());
+        });
     }
 }
